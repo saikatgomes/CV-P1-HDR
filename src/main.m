1a86686b5783d54f8a48f0530c5b6aed63908ae6
Stephen Lazzaro <slazzaro@knuckles.cs.wisc.edu>
2014-02-17 17:03:09 -0600

    fix createSmoothTonemap function

--- src/main.m
+++ src/main.m
@@ -3,11 +3,11 @@ function [pixelArray, newMap, logExposure1, logExposure2, logExposure3] = main(d
 %   Detailed explanation goes here
 
     [pixelArray,logT] = readImages(directory);
-    [logExposure1,irradiance1]=createHDR(pixelArray,logT, 1,1);
-    [logExposure2,irradiance2]=createHDR(pixelArray,logT, 1,2);
-    [logExposure3,irradiance3]=createHDR(pixelArray,logT, 1,3);
+    [logExposure1,irradiance1]=createHDR(pixelArray,logT, 0.5,1);
+    [logExposure2,irradiance2]=createHDR(pixelArray,logT, 0.5,2);
+    [logExposure3,irradiance3]=createHDR(pixelArray,logT, 0.5,3);
     [newMap] = createToneMap(directory,pixelArray,logExposure1,logExposure2,logExposure3,logT);
-    %[newMap] = createToneMap(directory,pixelArray,logExposure1,logExposure2,logExposure3,logT);
+    %[newMap] = createToneMapSmooth(directory,pixelArray,logExposure1,logExposure2,logExposure3,logT);
     
 end
 
