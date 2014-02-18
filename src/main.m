4a216dd835a95f802cf3956fac2b191a5b86c709
saikatgomes <saikat@cs.wisc.com>
2014-02-18 04:33:29 -0600

    restructured the tone-mapping code

--- src/main.m
+++ src/main.m
@@ -1,14 +1,7 @@
-function [pixelArray, newMap, logExposure1, logExposure2, logExposure3] = main(directory)
-%UNTITLED3 Summary of this function goes here
+function [  ] = main( directory )
+%UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-    [pixelArray,logT] = readImages(directory);
-    [logExposure1,irradiance1]=createHDR(pixelArray,logT, 1,1);
-    [logExposure2,irradiance2]=createHDR(pixelArray,logT, 1,2);
-    [logExposure3,irradiance3]=createHDR(pixelArray,logT, 1,3);
-    [newMap] = createToneMap(directory,pixelArray,logExposure1,logExposure2,logExposure3,logT);
-    %[newMap] = createToneMapSmooth(directory,pixelArray,logExposure1,logExposure2,logExposure3,logT);
-    
-end
 
+end
 
